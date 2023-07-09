bb7b839970243ac1cf3306eb88373f77d5274d3e
Don Ky <don.d.ky@gmail.com>
2023-07-08 21:29:02 -0400

    pass in legacy since macos uses openssl 3.0 and it's not compatible with keychain

--- combine.sh
+++ combine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl pkcs12 -export -clcerts -in client.crt -inkey client.key -out client.p12
+openssl pkcs12 -export -legacy -clcerts -in client.crt -inkey client.key -out client.p12
